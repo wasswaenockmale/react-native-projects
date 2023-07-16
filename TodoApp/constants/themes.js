786baef3d6ef61e81aff0ff829cf41f8cd00298a
wasswaenockmale <enockmale97@gmail.com>
2023-07-16 16:47:04 +0300

    themes.js

---
+++ TodoApp/constants/themes.js
@@ -0,0 +1,61 @@
+export const COLORS = {
+    WHITE: "#FFFFFF",
+    BLUE: "#2271B1",
+    YELLOW: "#FADB08",
+    GREEN: "#2A8E2C",
+    RED: "#AC0101",
+    GRAY: "#1D2327",
+    GREY:"#2C3338",
+    BLACK: "#000000"
+}
+
+export const FONT = {
+    bold: "RobotoBold",
+    // semibold: "RobotoSemiBold",
+    medium: "RobotoMedium",
+    regular: "RobotoRegular",
+    light: "RobotoLight",
+}
+
+export const SIZES = {
+    large: 20,
+    MEDIUM: 16,
+    small: 14,
+    icon_size: 30
+}
+
+export const SHADOWS = {
+    light: {
+      shadowColor: COLORS.gray,
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.22,
+      shadowRadius: 2.22,
+  
+      elevation: 3,
+    },
+    medium: {
+      shadowColor: COLORS.gray,
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.29,
+      shadowRadius: 4.65,
+  
+      elevation: 7,
+    },
+    dark: {
+      shadowColor: COLORS.gray,
+      shadowOffset: {
+        width: 0,
+        height: 7,
+      },
+      shadowOpacity: 0.41,
+      shadowRadius: 9.11,
+  
+      elevation: 14,
+    },
+  };
